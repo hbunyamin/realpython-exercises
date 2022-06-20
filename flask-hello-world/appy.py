fda13183bde2c97b969ad062f7853fe74f9650be
hbunyamin <hendra.bunyamin@it.maranatha.edu>
2022-06-20 07:20:47 +0700

    Route URL Links

--- flask-hello-world/appy.py
+++ flask-hello-world/appy.py
@@ -9,10 +9,36 @@ app = Flask(__name__)
 # use the decorator pattern to link the view function to a url
 @app.route("/")
 @app.route("/hello")
-
 # define the view using a function, which returns a string
 def hello_world():
 	return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+# route handler for integer
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value+1)
+	return "correct"
+
+# route handler for float
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value+1)
+	return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print(value)
+	return "correct"
+
+	
+
+
 	
 # start the development server using the run() method
 if __name__ == "__main__":
